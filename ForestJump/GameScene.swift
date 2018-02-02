c0a76f163f327b3cfb89011168731ceae58d7d26
vjbhaskar <vj.bhaskar012@gmail.com>
2018-02-02 12:21:13 +0100

    jump player on tap

--- ForestJump/GameScene.swift
+++ ForestJump/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+
     var Player = SKSpriteNode()
     var Ground = SKSpriteNode()
     var background = SKSpriteNode()
@@ -22,19 +22,19 @@ class GameScene: SKScene {
     var score = Int()
     var ScoreLabel = SKLabelNode()
 
-    
 
-    
+
+
     func createScene(){
-        
+
         // ================ Adding Score =============
         ScoreLabel.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + self.frame.height / 2.5)
         ScoreLabel.text = "Score \(score)"
         ScoreLabel.zPosition = 8
         self.addChild(ScoreLabel)
-        
+
         // ================ Adding background =============
-        
+
         for i in 0..<2{
             background = SKSpriteNode(imageNamed: "mountains_bg")
             background.anchorPoint = CGPoint(x:0,y:0)
@@ -42,26 +42,26 @@ class GameScene: SKScene {
             background.name = "background"
             self.addChild(background)
         }
-        
+
         // ============== Adding ground ===========
-        
+
         Ground = SKSpriteNode(imageNamed: "ground")
         Ground.setScale(0.75)
         Ground.position = CGPoint(x: self.frame.width/2, y: 0+Ground.frame.height/3)
         Ground.physicsBody = SKPhysicsBody(rectangleOf: Ground.size)
         Ground.physicsBody?.isDynamic = false
         Ground.zPosition = 5
-        
+
         self.addChild(Ground)
-        
+
         // ============== Texture for animating the player  ==============
-        
+
         TextureAtlas = SKTextureAtlas(named: "Player")
         for i in 1...TextureAtlas.textureNames.count{
             ImageName = "player\(i).png"
             TextureArray.append(SKTexture(imageNamed: ImageName!))
         }
-        
+
         // ============== Adding player ==============
         Player = SKSpriteNode(imageNamed: "player1.png")
         Player.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
@@ -69,32 +69,48 @@ class GameScene: SKScene {
         Player.physicsBody = SKPhysicsBody(circleOfRadius: Player.frame.height/2)
         Player.physicsBody?.isDynamic = true
         Player.physicsBody?.affectedByGravity = false
-        
+
         Player.zPosition = 8
         Player.physicsBody?.allowsRotation = false
         self.addChild(Player)
-        
+
     }
-    
+
     override func didMove(to view: SKView) {
-        
+
       createScene()
 
     }
-    
-   
-   
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-       
-      
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-       
-    }
-    
 
-    
+
+
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        // ============== Action for 2 seconds of time delay ============
+        let delay = SKAction.wait(forDuration: 2)
+
+        // ============== The above both actions are added in sequence to run ==========
+        let spawnDelay = SKAction.sequence([delay])
+
+        // ============== Action for repeating the above sequence forever ===========
+        let spawnDelayForever = SKAction.repeatForever(spawnDelay)
+
+        // ============== Running the spawndelayforever action =============
+        self.run(spawnDelayForever)
+
+        // ============== moving the player upwards when user tap on screen ============
+
+        Player.run(SKAction.repeatForever(SKAction.animate(with: TextureArray, timePerFrame: 0.1)))
+        Player.physicsBody?.velocity = CGVector(dx: 0,dy: 0)
+        Player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 100))
+
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+
+    }
+
+
+
 }
